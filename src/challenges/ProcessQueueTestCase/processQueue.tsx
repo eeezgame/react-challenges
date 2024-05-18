d42323aba3369572e8203850771ef2bd40f7fb3f
ZRQ30 <eeezgame@163.com>
2024-05-18 12:03:56 +0800

    finish challenge: ProcessQueueTestCase

--- src/challenges/ProcessQueueTestCase/processQueue.tsx
+++ src/challenges/ProcessQueueTestCase/processQueue.tsx
@@ -1,7 +1,18 @@
 export function getFinalState(baseState:number, queue: (Function | number)[]) {
     let finalState = baseState;
-  
+    let tasks =  queue.slice()
+
     // TODO: 对队列做些什么...
+    while (tasks.length) {
+        const task = tasks.shift()
+        const isFunc = typeof task === 'function';
+        const isNum = typeof task === 'number'
+        if(isFunc){
+            finalState = task(finalState)
+        } else if(isNum) {
+            finalState = task
+        }
+    }
   
     return finalState;
   }
